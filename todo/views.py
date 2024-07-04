543f6dae8bb928d1f8bf611f5b8d334cb8a7935e
s1F102300357 <s1f102300357@iniad.org>
2024-07-04 13:58:27 +0900

    警告を修正5

--- todo/views.py
+++ todo/views.py
@@ -12,9 +12,9 @@ def index(request):
 
     if request.GET.get('order') == 'due':
         tasks = Task.objects.order_by('due_at')
+
     else:
         tasks = Task.objects.order_by('-posted_at')
-
     context = {
         'tasks': tasks
     }
